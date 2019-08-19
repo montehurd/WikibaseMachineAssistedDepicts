22daa2f75a7ceac641267bb494b8284457872fe5
montehurd <mhurd@wikimedia.org>
2019-08-19 10:45:51 -0700

    simplify page construction

--- resources/widgets/ImageDepictsSuggestionsPage.js
+++ resources/widgets/ImageDepictsSuggestionsPage.js
@@ -6,24 +6,22 @@ var ImageWithSuggestionsWidget = require( './ImageWithSuggestionsWidget.js' );
 var	ImageDepictsSuggestionsPage = function WikibaseMachineAssistedDepictsImageDepictsSuggestionsPage( config ) {
 	ImageDepictsSuggestionsPage.parent.call( this, $.extend( {}, config ) );
 	this.$element.addClass('wbmad-image-depicts-suggestions-page');
-	this.imageDataArray = config.imageDataArray;
+	this.addItems($.map( config.imageDataArray, getImageWithSuggestionsWidgetForImageData ));
 	this.render();
 };
 OO.inheritClass( ImageDepictsSuggestionsPage, TemplateRenderingDOMLessGroupWidget );
 
 var getImageWithSuggestionsWidgetForImageData = function ( imageData ) {
-	return new ImageWithSuggestionsWidget({
+	return new ImageWithSuggestionsWidget( {
 		imageData: imageData
-	});
+	} );
 };
 
 ImageDepictsSuggestionsPage.prototype.render = function () {
-	var imageWithSuggestionsWidgets = $.map( this.imageDataArray, getImageWithSuggestionsWidgetForImageData );
-	this.addItems(imageWithSuggestionsWidgets);
 	this.renderTemplate(
 		'resources/widgets/ImageDepictsSuggestionsPage.mustache+dom',
 		{
-			imageWithSuggestionsWidgets: imageWithSuggestionsWidgets
+			imageWithSuggestionsWidgets: this.items
 		}
 	);
 };
