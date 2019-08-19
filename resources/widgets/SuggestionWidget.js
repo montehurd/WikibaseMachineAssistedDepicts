5dfa614893a8aaa04bdb055ec887699a427d7e42
montehurd <mhurd@wikimedia.org>
2019-08-19 11:23:42 -0700

    update var names

--- resources/widgets/SuggestionWidget.js
+++ resources/widgets/SuggestionWidget.js
@@ -5,16 +5,12 @@ var SuggestionBaseWidget = require( './SuggestionBaseWidget.js' );
 var	SuggestionWidget = function WikibaseMachineAssistedDepictsSuggestionWidget( config ) {
 	SuggestionWidget.parent.call( this, $.extend( {}, config ) );
 	this.$element.addClass('wbmad-suggestion');
-	this.render();
-};
-OO.inheritClass( SuggestionWidget, SuggestionBaseWidget );
 
-SuggestionWidget.prototype.render = function () {
-	var suggestionLabel = new OO.ui.LabelWidget( {
+	this.suggestionLabel = new OO.ui.LabelWidget( {
 		label: this.suggestionData.text
 	} );
 
-	var subtractButton = new OO.ui.ButtonWidget( {
+	this.subtractButton = new OO.ui.ButtonWidget( {
 		title: mw.message( 'wikibasemachineassisteddepicts-suggestion-reject-title', this.suggestionData.text ).text(),
 		icon: 'close',
 		framed: false
@@ -24,11 +20,16 @@ SuggestionWidget.prototype.render = function () {
 		click: this.emitConfirmSuggestion.bind( this )
 	} );
 
+	this.render();
+};
+OO.inheritClass( SuggestionWidget, SuggestionBaseWidget );
+
+SuggestionWidget.prototype.render = function () {
 	this.renderTemplate(
 		'resources/widgets/SuggestionWidget.mustache+dom',
 		{
-			suggestionLabel: suggestionLabel,
-			subtractButton: subtractButton
+			suggestionLabel: this.suggestionLabel,
+			subtractButton: this.subtractButton
 		}
 	);
 };
